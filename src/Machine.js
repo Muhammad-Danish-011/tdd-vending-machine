cf1de310ed85f4661f79d93b6dc3cefec3febac0
Muhammad Danish <muhammad.danish@xloopdigital.com>
2023-04-12 09:35:21 -0700

    fail test 1

--- src/Machine.js
+++ src/Machine.js
@@ -1,9 +1,14 @@
-module.exports = class Machine {
-    constructor() {
 
+
+//this the first test case story 
+
+
+function SeeItems() {
+    let items = [{'crisps': 'Rs 100'}, {'chocolate': 'Rs 350'}, {'mints': 'Rs 70'}];
+    for (let i = 0; i < items.length; i++) {
+     let Name = Object.keys(items[i])[0]; 
+      let Price = items[i][Name]; 
+      console.log(`${Name}: ${Price}`);
     }
-
-
-    seeSelections() {
-    }
-};
+  }
+   console.log(SeeItems());
--- test/Machine.test.js
+++ test/Machine.test.js
@@ -3,14 +3,20 @@ const Machine = require('../src/Machine');
 describe('the vending machine', () => {
     it('should have items to purchase', () => {
         // setup
-        const machine = new Machine();
-        const expected = [{'crisps': 100}, {'chocolate': 350}, {'mints': 70}];
+        function SeeItems() {
+            let items = [{'crisps': 'Rs 100'}, {'chocolate': 'Rs 350'}, {'mints': 'Rs 70'}];
+            for (let i = 0; i < items.length; i++) {
+             let Name = Object.keys(items[i])[0]; 
 
         // exercise
-        const actual = machine.seeSelections();
+        let Price = items[i][Name]; 
+      console.log(`${Name}: ${Price}`);
+    }
+  }
+   console.log(SeeItems());
 
         // assert
-        expect(true).toEqual(true);
+        expect(SeeItems()).toEqual(items);
     });
 
 });

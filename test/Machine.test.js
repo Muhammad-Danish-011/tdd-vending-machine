101153c4127c6e3379b4dac71fe8e570d06b94eb
Muhammad-Danish-011 <muhammad.danish@xloopdigital.com>
2023-05-01 10:14:09 -0500

    test case 1 fail

--- test/Machine.test.js
+++ test/Machine.test.js
@@ -1,28 +1,20 @@
 const Machine = require('../src/Machine');
 
-/*describe('the vending machine', () => {
+describe('the vending machine', () => {
     it('should have items to purchase', () => {
         // setup
         function SeeItems() {
-            let items = [{'crisps': 'Rs 100'}, {'chocolate': 'Rs 350'}, {'mints': 'Rs 70'}];
-            for (let i = 0; i < items.length; i++) {
-             let Name = Object.keys(items[i])[0]; 
-
-        // exercise
-        let Price = items[i][Name]; 
-      console.log(`${Name}: ${Price}`);
-    }
-  }
-   console.log(SeeItems());
+        }
+       console.log(SeeItems());
 
         // assert
-        expect(true).toEqual(true);
-    });
+        expect(false).toEqual(true);
+ });
 
 });
-*/
+
 // test case of 2nd story
-/* describe('the vending machine', () => {
+  /*describe('the vending machine', () => {
   it('should have items to purchase', () => {
       // setup
       let total = 0;
